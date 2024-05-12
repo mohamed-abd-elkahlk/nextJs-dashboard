59aa72a0c758f0bee276ccbad5b8f58fa67369bf
mohamed abd elkhalk <01147018091.m@gmial.com>
2024-05-13 01:46:59 +0300

    warp custmour table with suspense boundry

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,15 @@
 import { fetchFilteredCustomers } from '@/app/lib/data';
 import CustomersTable from '@/app/ui/customers/table';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 
 const page = async () => {
   const customers = await fetchFilteredCustomers('');
   return (
     <div>
-      <CustomersTable customers={customers} />
+      <Suspense fallback={<TableRowSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
     </div>
   );
 };
